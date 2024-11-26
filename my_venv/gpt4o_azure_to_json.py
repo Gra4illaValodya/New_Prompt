86bb6ee58de3d0ddf9847a9e380dfd27cfa88ff8
Vova <gra4illa@gmail.com>
2024-11-26 15:16:30 +0200

    money_spared_on_the_card and  offer_without_price_loyalty_card

--- my_venv/gpt4o_azure_to_json.py
+++ my_venv/gpt4o_azure_to_json.py
@@ -1,7 +1,7 @@
 import openai
 import base64
 import re
-from text_prompts_txt import one_price
+from text_prompts_txt import size_chart
  
 openai.api_type = "azure"   
 openai.api_version = "2023-05-15"       
@@ -42,7 +42,7 @@ def chat_with_gpt4(prompt, image_path, model="bonial-gpt-4o", max_tokens=4096):
                            }
                        ]
                        }
-                      ],
+                      ],    
             max_tokens=max_tokens,
             temperature=0.1
         )
@@ -60,27 +60,22 @@ def chat_with_gpt4(prompt, image_path, model="bonial-gpt-4o", max_tokens=4096):
         return f"Error: {e}"    
 
 
-prompt = one_price
-image_path = "https://content-media.bonial.biz/102aa539-8c6e-4cc0-a4b8-1b613a399485/main.jpg"
+prompt = size_chart
+image_path = "https://content-media.bonial.biz/ec808ec8-191a-44c0-8dbe-f793fc96e80f/main.jpg"
 #image_path = "vine__.jpg"
 response = chat_with_gpt4(prompt, image_path)                    
 print(response)
 
-# 1. https://content-media.bonial.biz/59f93948-c450-4add-a053-d48a3d76e824/main.jpg
-# 2. https://content-media.bonial.biz/60a64f1c-6d09-4375-87d9-0f7d9c7ac11d/main.jpg
-# 3. https://content-media.bonial.biz/14231501-a580-47ab-b4fe-cf3e42507013/main.jpg
-# 4. https://content-media.bonial.biz/bfb7ed95-50af-4ed4-8647-85a6421572e0/main.jpg
-# 5. https://content-media.bonial.biz/8d458ab6-c0b7-4f87-b36e-855a5a770bf0/main.jpg
-# 6. https://content-media.bonial.biz/03976572-4ad7-43c1-8e33-a57a1f4687ec/main.jpg
-# 7. https://content-media.bonial.biz/8384861d-6a36-42bd-9578-6548c3000a3e/main.jpg
-# 8. https://content-media.bonial.biz/0fc755ed-c3ea-46da-8bd3-df0cd8c0aa27/main.jpg
-# 9. https://content-media.bonial.biz/102aa539-8c6e-4cc0-a4b8-1b613a399485/main.jpg
-# 10. https://content-media.bonial.biz/b798d917-49ea-4b2c-bbc8-4639147e446f/main.jpg
-# 11. https://content-media.bonial.biz/09552598-8138-4bed-bfa2-08c7d623ef26/main.jpg
-# 12. https://content-media.bonial.biz/474d1f67-7ee8-4a0b-8dca-f745c56f1f17/main.jpg
-# 13. https://content-media.bonial.biz/df50a8ae-07f6-4c7d-bdcb-b5d5cb6e7e67/main.jpg
-
-
+    # 1. https://content-media.bonial.biz/ec808ec8-191a-44c0-8dbe-f793fc96e80f/main.jpg
+    # 2. https://content-media.bonial.biz/b2580888-4eb3-403e-b543-be1400b11588/main.jpg
+    # 3. https://content-media.bonial.biz/38190d46-b241-4017-85f2-0b2b43173b94/main.jpg
+    # 4. https://content-media.bonial.biz/b889484d-7dcd-41d2-ae8b-973db1646bd6/main.jpg
+    # 5. https://content-media.bonial.biz/8ac819fe-2aac-46bb-9092-d39625525563/main.jpg  тут що б не робив 1 діл не дораховує 
+    # 6. https://content-media.bonial.biz/fedbced9-b555-4d7a-bb4c-a84c5daexc4c4/main.jpg
+    # 7. https://content-media.bonial.biz/69c53b29-6087-4194-8884-d2dda11f1889/main.jpg
+    # 8. https://content-media.bonial.biz/8985d082-baa2-4ed7-8899-dee33a56fdc7/main.jpg
+    # 9. https://content-media.bonial.biz/8dd07775-9721-442e-9eb0-8abeca92d055/main.jpg
+    # 10. https://content-media.bonial.biz/0609ba05-52d1-4fd4-819f-67a0051a7267/main.jpg.      
 #FR
 # one_price
 # one_old_and_one_new_price
--- my_venv/tempCodeRunnerFile.py
+++ my_venv/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-import openai
+biz/9eeece72-a98f-416e-acb9-bc9752d49679/main.jpg
+# 3. https://content-media.bonial.
